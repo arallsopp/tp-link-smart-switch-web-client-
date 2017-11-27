c054a867bf4eef120ecc5eacd067cca725de4622
Andy Allsopp <andyallsopp@afinium.local>
2017-11-27 00:33:25 +0000

    generate UUID

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ app.config(function ($mdThemingProvider) {
 
 app.controller('dash', function ($scope, $mdToast, $http, $interval, $sce) {
 
+    $scope.getUUID = function(){
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        )
+    };
+
+    $scope.UUID = $scope.getUUID();
+
     $scope.showToast = function (msg) {
         $mdToast.show($mdToast.simple().textContent(msg).position('top right'))
     };
