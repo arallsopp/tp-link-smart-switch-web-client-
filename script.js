6d1ee3967ecf960e2671d33a185380caab76bad6
Andy Allsopp <andyallsopp@afinium.local>
2017-11-27 01:16:57 +0000

    auhenticates against tplink api to get a token

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var app = angular.module('myApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial','ngCookies']);
 
 app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('custom').primaryPalette('blue-grey').accentPalette('deep-orange');
 });
 
-app.controller('dash', function ($scope, $mdToast, $http, $interval, $sce) {
+app.controller('dash', function ($scope, $mdToast, $http, $interval, $sce, $cookies) {
 
     $scope.getUUID = function(){
         return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
@@ -17,12 +17,38 @@ app.controller('dash', function ($scope, $mdToast, $http, $interval, $sce) {
     };
 
     $scope.authenticate = function(){
-        alert('authenticate');
+        $scope.UUID=$scope.getUUID();
+
+        var auth_obj = {
+            "method": "login", "params": {
+                "appType": "Kasa_Android",
+                "cloudUserName": $scope.username,
+                "cloudPassword": $scope.password,
+                "terminalUUID": $scope.UUID
+            }
+        };
+
+        $http.post("https://wap.tplinkcloud.com/",auth_obj)
+        .then(function mySuccess(response) {
+            window.response = response;
+            $scope.token = response.data.result.token;
+
+            $cookies.put('uuid',$scope.UUID);
+            $cookies.put('username',$scope.username);
+            $cookies.put('password',$scope.password);
+            $cookies.put('token',$scope.token);
+
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+
+
+
     };
 
-    $scope.UUID = $scope.getUUID();
-
-
-
+    $scope.UUID = $cookies.get('uuid');
+    $scope.username = $cookies.get('username');
+    $scope.password = $cookies.get('password');
+    $scope.token = $cookies.get('token') | '';
 
 });
